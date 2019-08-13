08472f7245c1492c2a7b2e1821e5eb19acde8bcd
arjunQ21 <be2018se604@gces.edu.np>
2019-08-13 13:46:11 +0545

    added grid layout:

---
+++ layout.js
@@ -0,0 +1,5 @@
+// var leftRightCont = document.getElementById("lrcont") ;
+// var right = document.getElementsByClassName("right")[0] ;
+// var left = document.getElementsByClassName("left")[0] ;
+// leftRightCont.style.height = ((right.offsetHeight > left.offsetHeight ) ? right.offsetHeight : left.offsetHeight) + "px" ;
+// // console.log(leftRightCont, right);
